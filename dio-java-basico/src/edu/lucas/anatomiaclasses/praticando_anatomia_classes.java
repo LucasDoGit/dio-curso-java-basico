f4163444edc4ba15dd09abff59903fe01288b151
Lucas <lucas.saiz19@gmail.com>
2022-12-29 00:14:43 -0300

    ajustando o nome do pacote e descrição do código

---
+++ dio-java-basico/src/edu/lucas/anatomiaclasses/praticando_anatomia_classes.java
@@ -0,0 +1,30 @@
+package edu.lucas.anatomiaclasses;
+/*
+ * Aluno: Lucas Thiago Saiz Timoteo
+ * Data: 28/12/2022
+ * 
+ * 	criando o primeiro codigo +
+ *	praticando anatomia de classes
+ */
+public class praticando_anatomia_classes {
+
+	public static void main(String[] args) {
+		
+		int mediaFinal = 5;
+		
+		if (mediaFinal <6)
+			System.out.println("REPROVADO!");
+		else if (mediaFinal == 6)
+			System.out.println("PROVA MINERVA");
+		else
+			System.out.println("APROVADO");
+		
+		System.out.println("FIM PROGRAMA.");
+		
+		double  salarioMedio = 1500.23; //nao recomendado abreviar variaveis
+		String email = "aluno@escola.com"; //variaveis no singular
+		String [] emails = {"aluno@escola.com","professor@escola.com"}; //arrays em plural
+		String meuNome = "JOSEPH"; //utilizar um idioma mesmo com entradas em outros idiomas
+	}
+
+}
