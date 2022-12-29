c230be8acc313062dac4b76a095c6849bfcf4bb7
Lucas <lucas.saiz19@gmail.com>
2022-12-29 00:01:41 -0300

    primeiro codigo em java e praticando anatomia de classes

---
+++ dio-java-basico/src/edu/lucas/anatomia/classes/praticando_anatomia_classes.java
@@ -0,0 +1,29 @@
+package edu.lucas.anatomia.classes;
+/*
+ * Aluno: Lucas Thiago Saiz Timoteo
+ * Data: 28/12/2022
+ * 
+ *	praticando anatomia de classes
+ */
+public class praticando_anatomia_classes {
+
+	public static void main(String[] args) {
+		
+		int mediaFinal = 5;
+		
+		if (mediaFinal <6)
+			System.out.println("REPROVADO!");
+		else if (mediaFinal == 6)
+			System.out.println("PROVA MINERVA");
+		else
+			System.out.println("APROVADO");
+		
+		System.out.println("FIM PROGRAMA.");
+		
+		double  salarioMedio = 1500.23; //nao recomendado abreviar variaveis
+		String email = "aluno@escola.com"; //variaveis no singular
+		String [] emails = {"aluno@escola.com","professor@escola.com"}; //arrays em plural
+		String meuNome = "JOSEPH"; //utilizar um idioma mesmo com entradas em outros idiomas
+	}
+
+}
