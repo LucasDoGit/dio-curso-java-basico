1e89c5c201c9111897aeb8526fea9b7038d9bd98
Lucas <lucas.saiz19@gmail.com>
2023-01-20 19:58:56 -0300

    praticando estruturas da repetição e metodos

--- dio-java-basico/src/edu/lucas/metodos/ex1/calculadora.java
+++ dio-java-basico/src/edu/lucas/metodos/ex1/calculadora.java
@@ -6,22 +6,22 @@ package edu.lucas.metodos.ex1;
  */
 public class calculadora {
 
-	public static void soma(double numero1, double numero2) {
+	public static void soma(double numero1, double numero2) { // metodo para somar dois valores
 		double resultado = numero1 + numero2;
 		System.out.println("A soma de " + numero1 + " mais " + numero2 + " é: " + resultado);
 	}
 	
-	public static void subtracao(double numero1, double numero2) {
+	public static void subtracao(double numero1, double numero2) { // meotdo para fazer subtracao de de um valor por outro valor
 		double resultado = numero1 - numero2;
 		System.out.println("A subtração de " + numero1 + " menos " + numero2 + " é: " + resultado);
 	}
 	
-	public static void multipliacao(double numero1, double numero2) {
+	public static void multipliacao(double numero1, double numero2) { // multiplicacao entre valores
 		double resultado = numero1 * numero2;
 		System.out.println("A multiplicação de " + numero1 + " vezes " + numero2 + " é: " + resultado);
 	}
 	
-	public static void divisao(double numero1, double numero2) {
+	public static void divisao(double numero1, double numero2) { // divisao entre valores
 		double resultado = numero1 / numero2;
 		System.out.println("A divisao de " + numero1 + " por " + numero2 + " é: " + resultado);
 	}
--- dio-java-basico/src/edu/lucas/metodos/ex1/emprestimo.java
+++ dio-java-basico/src/edu/lucas/metodos/ex1/emprestimo.java
@@ -2,20 +2,20 @@ package edu.lucas.metodos.ex1;
 
 public class emprestimo {
 
-	public static int getDuasParcelas() {
+	public static int getDuasParcelas() { // metodo para se obter a quantidade duas parcelas
 		return 2;
 	}
-	public static int getTresParcelas() {
+	public static int getTresParcelas() { // metodo para se obter a quantidade tres parcelas
 		return 3;
 	}
-	public static double getTaxasDuasParcelas() {
+	public static double getTaxasDuasParcelas() { // metodo para se obter a taxa de duas parcelas
 		return 0.3;
 	}
-	public static double getTaxasTresParcelas() {
+	public static double getTaxasTresParcelas() { // metodo para se obter a taxa de tres parcelas
 		return 0.45;
 	}
 	
-	public static void calcular(double valor, int parcelas) {
+	public static void calcular(double valor, int parcelas) { // metodo que verifica as parecelas e calcula a taxa
 		
 		if (parcelas == 2) {
 			double valorFinal = valor + (valor * getTaxasDuasParcelas());
